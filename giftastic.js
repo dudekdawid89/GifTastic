47157b0edecdc9c7d821b2c07a076c2d2e4f4ddd
dudekdawid89 <dudekdawid89@gmail.com>
2019-06-03 23:55:27 -0500

    upadate functions

--- giftastic.js
+++ giftastic.js
@@ -1,9 +1,9 @@
-var topics = ["Dingo", "Panther","Bison","Cape Buffalo","Moose","Oryx","Bobcat","Gorilla","Musk Deer","Tahr","Koala","Mouflon","Sea Otter"]
+var topics = ["Dingo", "Panther", "Bison", "Cape Buffalo", "Moose", "Oryx", "Bobcat", "Gorilla", "Musk Deer", "Tahr", "Koala", "Mouflon", "Sea Otter"]
 
-function renderButton(){
+function renderButton() {
     $("#button-view").empty();
 
-    for(var i = 0; i < topics.length; i++){
+    for (var i = 0; i < topics.length; i++) {
         var newButton = $("<button>");
         newButton.addClass("animalClass");
         newButton.attr("data-animal", topics[i]);
@@ -12,7 +12,7 @@ function renderButton(){
     }
 }
 
-$("#add-animal").on("click", function(event){
+$("#add-animal").on("click", function (event) {
     event.preventDefault();
 
     var addAnimal = $("#aniaml-input").val().trim();
@@ -23,34 +23,35 @@ $("#add-animal").on("click", function(event){
 
 
 
-function display(){
+function display() {
     var animal = $(this).attr("data-animal");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +animal+ "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
 
     $.ajax({
         url: queryURL,
         method: "GET"
     })
-    .then(function(response){
+        .then(function (response) {
 
-        var results = response.data;
-        console.log(response);
-        $("#animal-view").empty();
-        for(var i = 0; i < results.length; i++) {
-            var gifDiv = $("<div>");
-            var rating = results[i].rating;
-            var p = $("<p>").text("Rating: " + rating);
-            var animalImage = $("<img>");
-            animalImage.attr("src", results[i].images.fixed_height_still.url);
-            animalImage.attr("data-state", "still");
-           
-            gifDiv.append(p);
-            gifDiv.append(animalImage);
-            $("#animal-view").append(gifDiv);
+            var results = response.data;
+            console.log(response);
+            $("#animal-view").empty();
+            for (var i = 0; i < results.length; i++) {
+                var gifDiv = $("<div>");
+                var rating = results[i].rating;
+                var p = $("<p>").text("Rating: " + rating);
+                var animalImage = $("<img>");
+                animalImage.attr("src", results[i].images.fixed_height_still.url);
+                animalImage.attr("data-state", "still");
+                animalImage.addClass("animalgif");
 
-        }
+                gifDiv.append(p);
+                gifDiv.append(animalImage);
+                $("#animal-view").append(gifDiv);
 
-    });
+            }
+
+        });
 }
 
 $(document).on("click", ".animalClass", display);
@@ -58,14 +59,17 @@ $(document).on("click", ".animalClass", display);
 
 renderButton();
 
+$(window).on("click", function () {
+    var state = $("img").attr("data-state");
+    for (var i = 0; i < results.length; i++) {
+        if (state === "still") {
+            $(this).attr("src", results[i].images.fixed_width.url)
+            $(this).attr("data-state", "animate");
+        } else {
+            $(this).attr("src", results[i].images.fixed_height_still.url)
+            $(this).attr("data-state", "still");
+        }
+    }
 
-    // var state = $(this).attr("data-state");
-    // for(var i = 0; i < results.length; i++) {
-    // if(state === "still"){
-    //     $(this).attr("scr", results[i].images.fixed_width.url)
-    //     $(this).attr("data-state","animate");
-    // } else{
-    //     $(this).attr("scr",results[i].images.fixed_height_still.url)
-    //     $(this).attr("data-state","still");
-    // }
-    // }
+})
+
